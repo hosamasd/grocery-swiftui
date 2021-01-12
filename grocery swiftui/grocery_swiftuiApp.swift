aa28a62fec1ab4c0b0e0dcb39f11c13497d59cf9
hosam <hosamelmalt@gmail.com>
2021-01-12 08:25:07 +0200

    Initial Commit

---
+++ grocery swiftui/grocery_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocery_swiftuiApp.swift
+//  grocery swiftui
+//
+//  Created by hosam on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct grocery_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
